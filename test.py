0569d5fe525974de07be3a343d8dc3e20da99ad3
JonahChrzanowski <42982132+JonahChrzanowski@users.noreply.github.com>
2019-02-05 17:34:51 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+import pytest
+import requests
+from bs4 import BeautifulSoup
+
+def test():
+    r = requests.get('http://localhost:8080')
+    s = BeautifulSoup(r.text, 'html.parser')
+    print(s.title.string)
+    assert s.title.string == 'Hello World!' 
+
+test()
