ede9d40a0153a13291b59a1976beb908752e51b0
Tyler <tjo1858@rit.edu>
2019-03-12 16:13:52 -0400

    changes to authentication, sorta working

--- Activity 3/flask/app.py
+++ Activity 3/flask/app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
 import pymysql
+from werkzeug import generate_password_hash, check_password_hash
+from datetime import date
 
 app = Flask(__name__, template_folder='templates')
-#mysql = MySQL()
-#app.config['MYSQL_DATABASE_USER'] = 'admin'
-#app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-#app.config['MYSQL_DATABASE_DB'] = 'db'
-#app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
-#mysql.init_app(app)
 
 @app.route("/")
 def home():
@@ -17,10 +14,25 @@ def home():
 def login():
     username = request.form['username']
     password = request.form['password']
-    db=pymysql.connect(db='db', user='admin', password='root', host='127.0.0.1', cursorclass=pymysql.cursors.DictCursor)
-    cursor = db.cursor()
-    #cursor.callproc('adduser', (username, password))	
-    #return '{}\n{}'.format(username, password)
+    hashedpass = generate_password_hash(password)
+    conn = pymysql.connect('mysql', 'root', 'root', 'db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT EncryptedPass FROM users WHERE Username={}".format(username))
+    userpass = cursor.fetchone()
+    if userpass == 0:
+        cursor.execute("INSERT INTO users(Username, EncryptedPass, DateCreated) VALUES \
+                ({}, {}, {})".format(username, hashedpass, datetime.date()))
+        cursor.close()
+        conn.close()
+        return render_template('login.html')
+    if check_password_hash(hashedpass, userpass):
+        cursor.close()
+        conn.close()
+        return 'success!'
+
+    cursor.close()
+    conn.close()
+    return "unsuccessful!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
